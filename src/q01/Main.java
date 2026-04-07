7099104f100b709689d89c01101513fb7f0c5ff2
Rudra775why <154236457+Rudra775why@users.noreply.github.com>
2026-04-07 14:29:56 +0530

    Implement hollow diamond printing logic
    Added code to print Pascal's triangle for n = 4.

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -20,6 +20,26 @@ public class Main {
         //  *   *
         //   * *
         //    *
+        
+        int n = 4;
 
+        for (int i = 0; i <= n; i++) {
+
+            // print spaces
+            for (int space = 0; space < n - i; space++) {
+                System.out.print("  ");
+            }
+
+            int val = 1; // first value in every row is always 1
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(val + "    ");
+
+                // update value using optimized formula
+                val = val * (i - j) / (j + 1);
+            }
+
+            System.out.println();
+        }
     }
 }
