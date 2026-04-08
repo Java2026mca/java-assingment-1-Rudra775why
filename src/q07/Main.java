f97a6c2d01a1b8e8f8fae814cf03085c2d3e6921
Rudra775why <154236457+Rudra775why@users.noreply.github.com>
2026-04-08 09:47:46 +0530

    Refactor bubble sort and output formatting

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -1,23 +1,17 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
-       
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        
-    
         int swaps = 0;
         for (int i = 0; i < n - 1; i++) {
             boolean swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // Swap elements
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -25,22 +19,13 @@ public class Main {
                     swapped = true;
                 }
             }
-          
-            if (!swapped) {
-                break;
-            }
+            if (!swapped) break;
         }
-        
-        
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i]);
-            if (i < n - 1) {
-                System.out.print(" ");
-            }
+            if (i < n - 1) System.out.print(" ");
         }
         System.out.println();
         System.out.println("Swaps: " + swaps);
-        
-        sc.close();
     }
-} 
+}
